ff11dff7d1c8cf6665eedaae6c26ac3d39f8cd17
LeeMjnnkDzuy <duylelv17@gmail.com>
2025-06-24 00:30:03 +0700

    Remove unused useEffect import from EmailVerification component

--- src/pages/EmailVerification/index.jsx
+++ src/pages/EmailVerification/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {Link, useNavigate, useSearchParams} from "react-router-dom";
 import {FiArrowLeft} from "react-icons/fi";
 import {FaTree} from "react-icons/fa";
